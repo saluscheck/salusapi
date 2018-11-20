b86a158bc5340b1f532809241ded1c95c1ac5b66
witusj <wfth69@gmail.com>
2018-11-20 16:59:14 +0100

    Added table

--- public/query.js
+++ public/query.js
@@ -4,6 +4,8 @@ function getData() {
   // Bij deployment op Google App Engine poortnummer verwijderen - Bij testen op localhost toevoegen
   // https://salustest-7df6a.appspot.com/sanctions?search=
   // Online API kan niet in client worden gebruikt bij test op localhost vanwege CORS
+    
+    document.getElementById('example-table').innerHTML = ""
     var api_path = "https://salustest-7df6a.appspot.com/sanctions?search="
     var naam = document.getElementById("searchString").value
     var url = api_path + naam
@@ -11,32 +13,29 @@ function getData() {
     console.log(url)
     document.getElementById('result').innerHTML = '<div class="container">loading ....</div>';
     document.getElementById("loader").style.display = "inline";
+    document.getElementById('example-table').style.display = "none";
     fetch(url)
       .then((res) => { return res.json() })
       .then((data) => {
         if(data.length == 0) {
           console.log("Empty")
-          document.getElementById("loader").style.display = "none";
-          document.getElementById('result').innerHTML = "Geen resultaten"; 
+          document.getElementById('loader').style.display = "none";
+          document.getElementById('result').innerHTML = "Geen resultaten";
         } else {
           let result = `<h2>Zoekresultaten (${data.length} items)</h2>`;
-            data.forEach((ind) => {
-              console.log(ind)
-            const { euReferenceNumber, nameAlias: {firstName, middleName, lastName, wholeName} } = ind
-            result +=
-              `<div class=row>
-                   <ul class="w3-ul">
-                       <li> euReferenceNumber : ${euReferenceNumber[0]}</li>
-                       <li> First : ${firstName[0]}</li>
-                       <li> Middle : ${middleName[0]}</li>
-                       <li> Last : ${lastName[0]}</li>
-                       <li> Whole : ${wholeName[0]}</li>
-                       <hr />
-                   </ul>
-                </div>`;
-                  document.getElementById("loader").style.display = "none";
-                  document.getElementById('result').innerHTML = result; //!! Test outside foreach
-              });
+          document.getElementById('result').innerHTML = result;
+          document.getElementById('example-table').style.display = "block";
+          var table = new Tabulator("#example-table", {
+            data:data, //set initial table data
+            layout:"fitColumns",
+            groupBy:"euReferenceNumber",
+            columns:[
+                {title:"Voornaam", field: "nameAlias.firstName"},
+                {title:"Achternaam", field: "nameAlias.lastName"},
+                {title:"Volledige naam", field: "nameAlias.wholeName"},
+            ],
+        });
+        document.getElementById('loader').style.display = "none";
   }
 })
 }
