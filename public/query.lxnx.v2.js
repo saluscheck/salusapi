4ed36e1a873414c56326ccd230ae75f95e331781
witusj <wfth69@gmail.com>
2019-01-06 13:32:07 +0100

    Update

--- public/query.lxnx.v2.js
+++ public/query.lxnx.v2.js
@@ -1,4 +1,4 @@
-document.getElementById('getData').addEventListener('click', retResults);
+document.getElementById('getData').addEventListener('click', getData);
 document.getElementById('getPDF').addEventListener('click', getPDF);
 
 async function getData(url) {
@@ -18,58 +18,16 @@ async function getData(url) {
 
 }
 
-async function retResults() {
-  var api_path = "https://salustest-7df6a.appspot.com/lxnx?search="
-  var naam =   "rutte" //document.getElementById("Name").value
-  var url = api_path + naam
-  console.log(naam)
-  console.log(url)
-  getData(url)
-  .then(data => {
-    console.log(data)
-    let result = 
-            `
-              <div class="container">
-                <h2>Zoekresultaten (${ data.length } items)</h2>
-                <table class="table table-hover">
-                <thead>
-                    <tr>
-                      <th>Bron</th>
-                      <th>Voor</th> 
-                      <th>Naam</th>
-                      <th>Geboortedatum</th>
-                      <th>Nationaliteit</th>
-                      <th>Reden</th>
-                    </tr>
-                </thead>
-                <tbody>
-            `
-          data.forEach((ind) => {
-            
-          const { Forename, Name, BirthDate, Nationality, Reason, ListName } = ind
-          result +=
-            `
-                    <tr class="w3-ul">
-                        <td> ${ ListName }</td>
-                        <td> ${ Forename }</td>
-                        <td> ${ Name }</td>
-                        <td align="right"> ${ BirthDate }</td>
-                        <td> ${ Nationality }</td>
-                        <td> ${ Reason }</td>
-                    </tr>
-            `
-            })
-            result +=
-            `
-                  </tbody>
-                </table>
-            </div>  
-            `
-            document.getElementById("loader").style.display = "none";
-            document.getElementById('result').innerHTML = result; 
-
-  })
-  .catch(reason => console.log(reason.message))
-} 
-
+  
+var api_path = "https://salustest-7df6a.appspot.com/lxnx?search="
+    var naam =   "rutte" //document.getElementById("Name").value
+    var url = api_path + naam
+    console.log(naam)
+    console.log(url)
+    getData(url)
+    .then(data => {
+      console.log(data)
+      
+    })
+    .catch(reason => console.log(reason.message))
 
